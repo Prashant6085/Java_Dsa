79ec56c7c407eb4e916d0542d71cfa02bd8b23b9
Prashant Kumar <prashantkumar1521998@gmail.com>
2022-10-04 06:59:48 +0530

    usrinput

---
+++ src/UsrInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UsrInput {
+
+        public static void main(String[] args) {
+
+            Scanner inp= new Scanner(System.in);
+            int a= inp.nextInt();
+            System.out.println(a);
+        }
+    }
+
